1f0e1e0e71f274f4205d966339d0a9ea3210804a
ICCHOI <cicblack@ks.ac.kr>
2020-03-08 15:36:58 +0900

    a

--- src/OddAndEvenNumber.java
+++ src/OddAndEvenNumber.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+class OddAndEvenNumber {
+    public String solution(int num) {
+        String answer;
 
-public class OddAndEvenNumber {
+        if( num < 0 ) num = num * -1;
+
+        if ( (num % 2) == 1 ) return answer = "Odd";
+        else return answer = "Even";
+
+    }
+}
+
+class Main8 {
+    public static void main(String[] args) {
+        OddAndEvenNumber number = new OddAndEvenNumber();
+        System.out.println(number.solution(-1));
+    }
 }
--- src/TakeMiddleLetters.java
+++ src/TakeMiddleLetters.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class TakeMiddleLetters {
+    public String solution(String s) {
+        String answer;
 
-public class ReturnMiddleLetters {
+        if((s.length()%2)==1){
+            answer = s.substring((s.length()-1)/2,((s.length()-1)/2)+1);
+        }
+        else{
+            answer = s.substring(((s.length()-1)/2),((s.length()-1)/2)+2);
+        }
+        return answer;
+    }
+}
+
+class Main7{
+    public static void main(String[] args) {
+        TakeMiddleLetters letters = new TakeMiddleLetters();
+        System.out.println(letters.solution("qwer"));
+    }
 }
