cdc817eeab9875405957f93158e01b2d8b7fda0d
ICCHOI <cicblack@ks.ac.kr>
2020-03-09 13:54:03 +0900

    Solve 9 Problems

--- src/OddAndEvenNumber.java
+++ src/OddAndEvenNumber.java
@@ -1,3 +1,6 @@
+// 짝수와 홀수 https://programmers.co.kr/learn/courses/30/lessons/12937
+// 정수가 짝수 일 경우 Odd 홀수 일 경우 Even을 반환합니다.
+
 class OddAndEvenNumber {
     public String solution(int num) {
         String answer;
--- src/StringToInteger.java
+++ src/StringToInteger.java
@@ -1,5 +1,5 @@
 // 문자열을 정수로 바꾸기 https://programmers.co.kr/learn/courses/30/lessons/12925
-//입력받은 1-5자리 문자열을 맨 앞의 +, - 기호에 따라 정수로 바꾸시오.
+// 입력받은 1-5자리 문자열을 맨 앞의 +, - 기호에 따라 정수로 바꾸시오.
 
 class StringToInteger {
     public static int solution(String s) {
