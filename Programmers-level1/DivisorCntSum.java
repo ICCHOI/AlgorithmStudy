7d392bc7552d50544ca067dfee690a9f00be47e1
ICCHOI <cicblack@ks.ac.kr>
2021-11-08 00:26:41 +0900

    Solve DivisorCntSum.java

---
+++ Programmers-level1/DivisorCntSum.java
@@ -0,0 +1,21 @@
+/* 약수의 개수와 덧셈 https://programmers.co.kr/learn/courses/30/lessons/77884
+    left와 right까지의 모든 수들 중에 약수의 개수가 짝수인 수는 더하고 홀수인 수는 뺀 수를 반환하시오.
+ */
+class DivisorCntSum {
+    public int getCount(int div) {
+        int count = 0;
+        for (int i = 1; i <= div; i++) {
+            if (div % i == 0) count += 1;
+        }
+        return count;
+    }
+
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            if (getCount(i) % 2 == 0) answer += i;
+            else answer -= i;
+        }
+        return answer;
+    }
+}
