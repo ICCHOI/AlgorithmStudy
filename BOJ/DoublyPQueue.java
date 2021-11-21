c7e4f861315c79fc415f267f442ada5754acada8
ichoi <ichoi@student.42seoul.kr>
2021-11-21 15:32:55 +0900

    4% DoublyPQueue.java

--- BOJ/DoublyPQueue.java
+++ BOJ/DoublyPQueue.java
@@ -1,43 +1,60 @@
 import java.io.*;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class DoublyPQueue {
+    static Map<Integer, Integer> map = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder stringBuilder = new StringBuilder();
         int number = Integer.parseInt(bufferedReader.readLine());
 
         for (int i = 0; i < number; i++) {
-            StringBuilder stringBuilder = new StringBuilder();
             int calc = Integer.parseInt(bufferedReader.readLine());
             PriorityQueue<Integer> priorityQueueASC = new PriorityQueue<>();   // 123
             PriorityQueue<Integer> priorityQueueDSC = new PriorityQueue<>(Collections.reverseOrder());  // 321
+            map = new HashMap<>();
 
             for (int j = 0; j < calc; j++) {
                 StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
                 String letter = stringTokenizer.nextToken();
+
                 if (letter.equals("I")) {
                     int input = Integer.parseInt(stringTokenizer.nextToken());
                     priorityQueueASC.add(input);
                     priorityQueueDSC.add(input);
+                    map.put(input, map.getOrDefault(input, 0) + 1);
                 } else if (letter.equals("D")) {
                     int check = Integer.parseInt(stringTokenizer.nextToken());
-                    if (check == 1 && !priorityQueueDSC.isEmpty()) {
-                        int toDelete = priorityQueueDSC.poll();
-                        priorityQueueASC.remove(toDelete);
-                    } else if (check == -1 && !priorityQueueASC.isEmpty()) {
-                        int toDelete = priorityQueueASC.poll();
-                        priorityQueueDSC.remove(toDelete);
-                    }
+
+                    if (map.size() == 0) continue;
+                    if (check == 1) deleteMap(priorityQueueDSC);
+                    else deleteMap(priorityQueueASC);
                 }
             }
-            if (priorityQueueASC.isEmpty()) stringBuilder.append("EMPTY").append('\n');
-            else stringBuilder.append(priorityQueueDSC.poll()).append(' ').append(priorityQueueASC.poll()).append('\n');
-            bufferedWriter.write(stringBuilder.toString());
-            bufferedWriter.flush();
+            if (map.size() == 0) stringBuilder.append("EMPTY\n");
+            else {
+                int input = deleteMap(priorityQueueDSC);
+                stringBuilder.append(input).append(' ');
+                if(map.size() > 0) input = deleteMap(priorityQueueASC);
+                stringBuilder.append(input).append('\n');
+            }
         }
-        bufferedWriter.close();
+        System.out.println(stringBuilder);
+    }
+
+    static int deleteMap (PriorityQueue<Integer> queue) {
+        int toDelete;
+
+        while(true) {
+            toDelete = queue.poll();
+
+            int count = map.getOrDefault(toDelete, 0);
+            if(count ==0) continue;
+            if(count ==1) map.remove(toDelete);
+            else map.put(toDelete, count - 1);
+            break;
+        }
+        return toDelete;
     }
 }
